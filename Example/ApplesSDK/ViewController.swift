653d72a8894bd7c2c4fcfdb7c5e91d94dae60386
Vlad Buhaescu <vlad.buhaescu@qoobiss.com>
2024-05-24 15:02:12 +0300

    Initial commit

---
+++ Example/ApplesSDK/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ApplesSDK
+//
+//  Created by Vlad Buhaescu on 05/24/2024.
+//  Copyright (c) 2024 Vlad Buhaescu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
